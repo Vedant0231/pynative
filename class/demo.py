5d7f94650df1d68d7145ebbbc0a073a263db242c
Vedant0231 <vedant.chaudhari.777@gmail.com>
2022-12-02 17:01:36 +0530

    file added

---
+++ class/demo.py
@@ -0,0 +1,55 @@
+class common:
+    def commonfunction(self):
+        print("this are used for traveling")
+
+
+
+
+
+
+class vehicals(common):
+    def __init__(self, name, color, price, year):
+        self.name=name
+        self.color=color
+        self.price=price
+        self.year=year
+    
+    def bike(self):
+        print("bike name is:  " ,self.name)
+        print("bike color is: " ,self.color)
+        print("bike price is: " ,self.price)
+        print("bike year  is: " ,self.year)
+
+    def car(self):
+        print("car name is:  " ,self.name)
+        print("car color is: " ,self.color)
+        print("car price is: " ,self.price)
+        print("car year  is: " ,self.year)
+
+vehical1 = vehicals("hunter360","red","200000","2022")
+vehical1.bike()
+vehical1.commonfunction()
+print(vehical1)
+
+vehical2 = vehicals("swift","black","800000","2020")
+vehical2.car()
+print(vehical2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
