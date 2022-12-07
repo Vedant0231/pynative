b2a47e34637d4443350b0b52d149a1bef3a94222
Vedant0231 <106227621+Vedant0231@users.noreply.github.com>
2022-12-07 15:40:48 +0530

    Update pythondb.py

--- pythondb/pythondb.py
+++ pythondb/pythondb.py
@@ -1,8 +1,9 @@
+#By this program we can save user input data in mysql database
 import mysql.connector
 username= input("enter your name: ")
 useraddress= input("enter your address: ")
 try:
-    connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
+    connection = mysql.connector.connect(host="localhost",database="your database name",user="your user name",password="your password")
     insert_data="insert into demotable values('{}','{}')".format(username,useraddress)
     cursor=connection.cursor()
     cursor.execute(insert_data)
@@ -11,16 +12,4 @@ try:
 except mysql.connector.Error as error:
     print("dear",'{}',"your data didn't saved".format(username))    
 
-# """DELETE FROM demotable WHERE name='vedant,deep'"""
-# """select * from  users where name='{}' and address='{}'"""
 
-
-# try:
-#     connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
-#     insert_data="""DELETE FROM demotable WHERE name='vedant'"""
-#     cursor=connection.cursor()
-#     cursor.execute(insert_data)
-#     connection.commit()
-#     print("done")
-# except mysql.connector.Error as error:
-#     print(" not done")
