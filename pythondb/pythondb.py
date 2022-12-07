03be967d9eb3907ed0bd399028edb6d25381a02c
Vedant0231 <vedant.chaudhari.777@gmail.com>
2022-12-07 16:12:45 +0530

    file added

--- pythondb/pythondb.py
+++ pythondb/pythondb.py
@@ -1,26 +1,31 @@
 import mysql.connector
 username= input("enter your name: ")
 useraddress= input("enter your address: ")
-try:
+userpassword = input("enter your password: ")
+option= input("what you want to do:  ")
+
+if option =="insert":
     connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
-    insert_data="insert into demotable values('{}','{}')".format(username,useraddress)
+    insert_data="insert into userdata values('{}','{}',{})".format(username,useraddress,userpassword)
     cursor=connection.cursor()
     cursor.execute(insert_data)
     connection.commit()
     print("dear",'{}'" your data is saved".format(username))
-except mysql.connector.Error as error:
-    print("dear",'{}',"your data didn't saved".format(username))    
+
+elif option == "delete":
+    connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
+    delete_data = "delete from userdata where name='{}' and password='{}'".format(username,userpassword)
+    cursor=connection.cursor()
+    cursor.execute(delete_data)
+    connection.commit()
+    print("dear",'{}'" your data is deleted".format(username))
+
+else:
+    print("we don't have this option")
+
+ 
 
 # """DELETE FROM demotable WHERE name='vedant,deep'"""
 # """select * from  users where name='{}' and address='{}'"""
 
 
-# try:
-#     connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
-#     insert_data="""DELETE FROM demotable WHERE name='vedant'"""
-#     cursor=connection.cursor()
-#     cursor.execute(insert_data)
-#     connection.commit()
-#     print("done")
-# except mysql.connector.Error as error:
-#     print(" not done")
