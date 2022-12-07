4069f92b24be86978c271e663ac137413c737cdf
Vedant0231 <vedant.chaudhari.777@gmail.com>
2022-12-07 16:22:51 +0530

    file addded

---
+++ pythondb/pythondb.py
@@ -0,0 +1,22 @@
+import mysql.connector
+username= input("enter your name: ")
+useraddress= input("enter your address: ")
+userpassword = input("enter your password: ")
+option= input("what you want to do:  ")
+if option =="insert":
+    connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
+    insert_data="insert into userdata values('{}','{}',{})".format(username,useraddress,userpassword)
+    cursor=connection.cursor()
+    cursor.execute(insert_data)
+    connection.commit()
+    print("dear",'{}'" your data is saved".format(username))
+elif option == "delete":
+    connection = mysql.connector.connect(host="localhost",database="demodb",user="vedant",password="vedant123")
+    delete_data = "delete from userdata where name='{}' and password='{}'".format(username,userpassword)
+    cursor=connection.cursor()
+    cursor.execute(delete_data)
+    connection.commit()
+    print("dear",'{}'" your data is deleted".format(username))
+else:
+    print("we don't have this option")
+
